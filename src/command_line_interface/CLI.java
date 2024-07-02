1b1070d908dce4897c0e17042667faa06c8c3112
shaya <niki.abramov@mail.huji.ac.il>
2024-07-02 13:30:24 +0300

    second commit

---
+++ src/command_line_interface/CLI.java
@@ -0,0 +1,18 @@
+package command_line_interface;
+
+public class CLI {
+    public CLI() {
+
+    }
+
+    public void run() {
+        
+    }
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Study at finite state machines (FSM)");
+        System.out.println("First step is creating a shell structure.");
+    }
+}
